939f78fdc69fd39506fc6fa44f59ea82e397392e
elianriu <sinagadarvin@gmail.com>
2026-01-08 20:53:10 +0700

    feat: about me content

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -54,10 +54,10 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: 'elianriu/elian-notes',
+      repositoryId: 'R_kgDOPcd2mw',
+      category: 'General',
+      categoryId: 'DIC_kwDOPcd2m84CuGWF',
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
